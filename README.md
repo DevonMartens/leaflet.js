1647d7b7c4797c91f53506f586fefb1a9720ee30
Devon Martens <devonmartens@Devons-MacBook-Pro-2.local>
2021-02-03 19:36:31 -0500

    changed location of map

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # leaflet.js
+
+
+An excellent rescource for mapping locations and finding corredence:
+ 
+http://geojson.io/#map=2/20.0/0.0
+
+ "coordinates": [
+          -84.7540283203125,
+          33.735760815044635
+        ]
--- leaf.js
+++ leaf.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([33.7357608150446359,  -84.7540283203125], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -10,34 +10,35 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
+// var circle = L.circle([51.508, -0.11], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(mymap);
 
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap);
+// var polygon = L.polygon([
+//     [51.509, -0.08],
+//     [51.503, -0.06],
+//     [51.51, -0.047]
+// ]).addTo(mymap);
+ 
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
+// // .openPopup();
+// circle.bindPopup("I am a circle.");
+// polygon.bindPopup("I am a polygon.");
+// var popup = L.popup()
+//     .setLatLng([51.5, -0.09])
+//     // .setContent("I am a standalone popup.")
+//     .openOn(mymap);
 
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
-var popup = L.popup()
-    .setLatLng([51.5, -0.09])
-    .setContent("I am a standalone popup.")
-    .openOn(mymap);
+//     var popup = L.popup();
 
-    var popup = L.popup();
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
-}
+// function onMapClick(e) {
+//     popup
+//         .setLatLng(e.latlng)
+//         .setContent("You clicked the map at " + e.latlng.toString())
+//       .openOn(mymap);
+// }
 
 mymap.on('click', onMapClick);
